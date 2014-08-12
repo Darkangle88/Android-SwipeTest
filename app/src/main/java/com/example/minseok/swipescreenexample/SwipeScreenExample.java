12112c915ce5ac9190028f1f2ff969ba792dd3d7
JinSeok Park <jinsuckelf@naver.com>
2014-08-12 23:41:59 +0900

    제작은 완성했으나, 테스트 필요함.

--- app/src/main/java/com/example/minseok/swipescreenexample/SwipeScreenExample.java
+++ app/src/main/java/com/example/minseok/swipescreenexample/SwipeScreenExample.java
@@ -2,19 +2,33 @@ package com.example.minseok.swipescreenexample;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class SwipeScreenExample extends Activity {
     Button testButton;
-    SwipeGestureListener gestureListener;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         testButton = (Button) findViewById(R.id.test_Button);
-        gestureListener = new SwipeGestureListener(SwipeScreenExample.this);
         testButton.setOnTouchListener(gestureListener);
     }
+
+    View.OnTouchListener gestureListener = new SwipeGestureListener()
+    {
+        public void BottomToTop(){
+        }
+
+        public void TopToBottom(){
+        }
+
+        public void RightToLeft(){
+        }
+
+        public void LeftToRight(){
+        }
+    };
 }
 
